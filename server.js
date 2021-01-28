00ccaaf22cdf19b82992de3a5f03712084a524ca
Beksultanova Dinara <beksultanovadinara@gmail.com>
2021-01-29 01:51:32 +0600

    change server

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router('./db.json');
+const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults({
     static: './build',
 });
 
-const PORT = process.env.PORT || 8000;
 
 server.use(middlewares);
 server.use(router);
 
-server.listen(PORT, () => {
-    console.log('Server is running')
-});
+server.listen(process.env.PORT || 3000, () => {
+    console.log('JSON Server is running')
+  });
