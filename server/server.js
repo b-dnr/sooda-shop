c4387cb1bc9ed4250368e3d831f78820ca43fd3b
Beksultanova Dinara <beksultanovadinara@gmail.com>
2021-01-29 21:22:19 +0600

    server deploy

--- server/server.js
+++ server/server.js
@@ -1,14 +1,11 @@
-const jsonServer = require('json-server');
-const server = jsonServer.create();
-const router = jsonServer.router('db.json');
-const middlewares = jsonServer.defaults({
-    static: './build',
-});
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router('db.json')
+const middlewares = jsonServer.defaults()
 
 
-server.use(middlewares);
-server.use(router);
-
+server.use(middlewares)
+server.use(router)
 server.listen(process.env.PORT || 3000, () => {
-    console.log('JSON Server is running')
-  });
+  console.log('JSON Server is running')
+});
